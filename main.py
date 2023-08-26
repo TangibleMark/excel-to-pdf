b66ad1a1912f03a29fad6e8f13490ed8712e2f3e
Tangimark <mark@teamtangible.com>
2023-08-26 17:21:36 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import openpyxl
+from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2.generic import BooleanObject, NameObject, IndirectObject
+import tkinter
+
+
+
+
+# PDF
+# Get fields
+# Open the original PDF
+
+
+
+
+
+
+
+ #16439
+
